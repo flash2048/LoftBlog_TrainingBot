4056efc165307fd0613a0b1bf2dbe6855856dc76
flash2048 <flash2048@gmail.com>
2017-09-15 22:43:17 +0300

    Fix bugs

---
+++ Lesson5/LoftBlog_TrainingBot/LoftBlog_TrainingBot/BotService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using LoftBlog_TrainingBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace LoftBlog_TrainingBot.obj
+{
+    public class BotService
+    {
+        public static void SendForUser(User user, string message)
+        {
+            if (user != null)
+            {
+                var client = new ConnectorClient(new Uri(user.ServiceUrl));
+
+                var userAccount = new ChannelAccount(user.UserId, user.UserName);
+                var botAccount = new ChannelAccount(user.FromId, user.FromName);
+
+                var activity = new Activity
+                {
+                    Conversation = new ConversationAccount(id: user.UserId),
+                    ChannelId = user.ChannelId,
+                    From = userAccount,
+                    Recipient = botAccount,
+                    Text = message,
+                    Id = user.Conversation
+                };
+
+                var reply = activity.CreateReply(message);
+                if(!String.IsNullOrEmpty(activity.ServiceUrl)){
+                    MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+                }
+                MicrosoftAppCredentials.TrustServiceUrl(user.ServiceUrl);
+                client.Conversations.ReplyToActivity(reply);
+            }
+        }
+
+        public static void SendForAll(string message)
+        {
+            foreach (var user in DataModel.Users)
+            {
+                SendForUser(user, message);
+            }
+        }
+    }
+}
--- Lesson6/LoftBlog_TrainingBot/LoftBlog_TrainingBot/Commands/ForAll.cs
+++ Lesson6/LoftBlog_TrainingBot/LoftBlog_TrainingBot/Commands/ForAll.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using LoftBlog_TrainingBot.Interfaces;
-using LoftBlog_TrainingBot.obj;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 
